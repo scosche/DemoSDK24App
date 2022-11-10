f84e9990e0a2a4cb2977d78d1e9667d0339aaf79
Steffin Fox Griswold <steffing@npe-inc.com>
2022-11-10 09:04:26 -0600

    Added Export FIT file

--- Demo/workoutViewController.swift
+++ Demo/workoutViewController.swift
@@ -64,9 +64,66 @@ class workoutViewController: SchoscheViewController, UITableViewDelegate, UITabl
             switch success {
             case true:
                 print("download successful")
+                DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute: {
+                    let DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+                    
+                    let fileURL = DocumentDirURL.appendingPathComponent(fileName).appendingPathExtension("fit")
+                    var filesToShare = [Any]()
+                    print(fileURL)
+                    // Add the path of the file to the Array
+                    filesToShare.append(fileURL)
+                    
+                    // Make the activityViewContoller which shows the share-view
+                    let activityViewController = UIActivityViewController(activityItems: filesToShare, applicationActivities: nil)
+                    
+                    // Show the share-view
+                    self.present(activityViewController, animated: true, completion: nil)
+                })
+               
+                
             case false:
                 print("download unsuccessful")
             }
         }
     }
+}
+
+extension Data {
+    
+    /// Data into file
+    ///
+    /// - Parameters:
+    ///   - fileName: the Name of the file you want to write
+    /// - Returns: Returns the URL where the new file is located in NSURL
+    func dataToFile(fileName: String) -> NSURL? {
+        
+        // Make a constant from the data
+        let data = self
+        
+        // Make the file path (with the filename) where the file will be loacated after it is created
+        let filePath = getDocumentsDirectory().appendingPathComponent(fileName)
+        
+        do {
+            // Write the file from data into the filepath (if there will be an error, the code jumps to the catch block below)
+            try data.write(to: URL(fileURLWithPath: filePath))
+            
+            // Returns the URL where the new file is located in NSURL
+            return NSURL(fileURLWithPath: filePath)
+            
+        } catch {
+            // Prints the localized description of the error from the do block
+            print("Error writing the file: \(error.localizedDescription)")
+        }
+        
+        // Returns nil if there was an error in the do-catch -block
+        return nil
+        
+    }
+    
+    func getDocumentsDirectory() -> NSString {
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let documentsDirectory = paths[0]
+        return documentsDirectory as NSString
+    }
+    
 }
